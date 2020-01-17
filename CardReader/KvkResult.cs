e908ef0e3723ae41104470d0f2af618eae706b02
Kaupisch IT-Systeme GmbH <github@kaupisch.it>
2020-01-17 17:06:15 +0100

    - zurückgegebene Datenobjekte kommentiert

--- CardReader/KvkResult.cs
+++ CardReader/KvkResult.cs
@@ -51,60 +51,77 @@ namespace CardReader
 
 
 		public string this[byte tag]
-		{
-			get
-			{
-				string result;
-				return (this.values.TryGetValue(tag,out result)) ? result : null;
-			}
-		}
+			=>  (this.values.TryGetValue(tag,out string result)) ? result : null;
 
 
+
+
+		/// <summary> Gibt die Bezeichnung und Ort der ausstellenden Krankenkasse an. </summary>
 		public string KrankenKassenName
 			=> this[0x80];
 
+		/// <summary> Gibt die Kassennummer oder das Institutionskennzeichen an. </summary>
 		public string KrankenKassenNummer
 			=> this[0x81];
 
-		public string VertragskassenNummer
+		/// <summary> Gibt die VKNR ("Vertragskassennummer der Kassenärztlichen Vereinigungen") bzw. das WOP-Kennzeichen ("Wohnortprinzip") an. </summary>
+		public string VKNR
 			=> this[0x8F];
 
+		/// <summary> Gibt die Krankenversichertennummer an. </summary>
 		public string VersichertenNummer
 			=> this[0x82];
 
+		/// <summary> 
+		/// Gibt den Versichertenstatus an. 
+		/// 1 = Versicherungspflichtige und -berechtigte
+		/// 3 = Familienversicherte
+		/// 5 = Rentner in der Krankenversicherung der Rentner und deren familienversicherten Angehörige
+		/// </summary>
 		public string VersichertenStatus
 			=> this[0x83];
 
+		/// <summary> Gibt das Kennzeichen bei Teilnehmern an einem Disease-Management-Programm an. </summary>
 		public string StatusErgänzung
 			=> this[0x90];
 
+		/// <summary> Gibt den Titel (optional) an. </summary>
 		public string Titel
 			=> this[0x84];
 
+		/// <summary> Gibt den Vornamen an. </summary>
 		public string VorName
 			=> this[0x85];
 
+		/// <summary> Gibt den Namenszusatz (optional) an. </summary>
 		public string NamensZusatz_VorsatzWort
 			=> this[0x86];
 
+		/// <summary> Gibt den Familienname an.n </summary>
 		public string FamilienName
 			=> this[0x87];
 
+		/// <summary> Gibt das Geburtsdatum an. </summary>
 		public string GeburtsDatum
 			=> this[0x88];
 
+		/// <summary> Gibt die Anschrift (Straße & Hausnr.) an. </summary>
 		public string StraßenName_HausNummer
 			=> this[0x89];
 
+		/// <summary> Gibt den Ländercode (optional, wenn nicht vorhanden: Deutschland) an. </summary>
 		public string WohnsitzLänderCode
 			=> this[0x8A];
 
+		/// <summary> Gibt die Anschrift (Postleitzahl) an. </summary>
 		public string Postleitzahl
 			=> this[0x8B];
 
+		/// <summary> Gibt die Anschrift (Ort) an. </summary>
 		public string OrtsName
 			=> this[0x8C];
 
+		/// <summary> Gibt bei befristeter Gültigkeit der Karte den Monat des Fristablaufs an. Üblich bei ausreichend langer Mitgliedschaft sind Gültigkeitsdauern von fünf bis zehn Jahren oder auch mehr. </summary>	
 		public string GültigkeitsDatum
 			=> this[0x8D];
 	}
