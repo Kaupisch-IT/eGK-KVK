c55c626b961a7f8cc8d3f4d918a88c711637e38d
Kaupisch IT-Systeme GmbH <github@kaupisch.it>
2020-01-28 21:02:05 +0100

    - Überarbeitung der Auswertung der zurückgegebenen Status-Codes

--- KaupischIT.CardReader/CardTerminalClient.cs
+++ KaupischIT.CardReader/CardTerminalClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace KaupischIT.CardReader
@@ -40,64 +41,110 @@ namespace KaupischIT.CardReader
 		}
 
 
+
 		/// <summary> Kartenleser zurücksetzen </summary>
 		public void ResetCT()
 		{
 			this.ExecuteCommand(sad: 2,dad: 1,command: new byte[] { 0x20,0x11,0x00,0x00,0x00 })
-				.ExpectStatusBytes("9000","9500");
+				.CheckStatusBytes(new Dictionary<string,string>
+				{
+					{ "9000", "Reset successful" },
+					{ "9001", "Asynchronous ICC, reset successful" },
+					{ "6400", "Reset not successful" },
+				});
 		}
 
-
-		/// <summary> Kartenanforderung </summary>
-		public void RequestICC()
+		/// <summary> 
+		/// Kartenanforderung 
+		/// </summary>
+		public string RequestICC()
 		{
-			this.ExecuteCommand(sad: 2,dad: 1,command: new byte[] { 0x20,0x12,0x01,0x00,0x00 })
-				.ExpectStatusBytes("9000","9001","6200","6201").GetStatusBytes();
+			return this.ExecuteCommand(sad: 2,dad: 1,command: new byte[] { 0x20,0x12,0x01,0x00,0x00 })
+				.CheckStatusBytes(new Dictionary<string,string>
+				{
+					{ "9000", "Synchronous ICC presented, reset successful" },
+					{ "9001", "Asynchronous ICC presented, reset successful" },
+					{ "6200", "Warning: no card presented within specified time" },
+					{ "6201", "Warning: ICC already present and activated" },
+					{ "6400", "Error: Reset not successful" },
+					{ "6401", "Error: Process aborted by pressing of cancel key" },
+					{ "6900", "Error: Command with timer not supported" },
+				})
+				.GetStatusBytes();
 		}
 
 		/// <summary> Karte auswerfen </summary>
-		public void EjectICC()
+		public string EjectICC()
 		{
-			this.ExecuteCommand(sad: 2,dad: 1,command: new byte[] { 0x20,0x15,0x01,0x00 })
-				.ExpectStatusBytes("9000","9001","6200");
+			return this.ExecuteCommand(sad: 2,dad: 1,command: new byte[] { 0x20,0x15,0x01,0x00 })
+				.CheckStatusBytes(new Dictionary<string,string>
+				{
+					{ "9000", "Command successful" },
+					{ "9001", "Command successful, card removed" },
+					{ "6200", "Warning: Card not removed within specified time" },
+				})
+				.GetStatusBytes();
 		}
 
 
-		/// <summary> eGGK-Applikation selektieren </summary>
-		public void SelectEGK()
+		/// <summary> Selektieren der eGK-Chipkartenanwendung </summary>
+		public string SelectEGK()
 		{
-			this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xa4,0x04,0x0c,0x06,0xd2,0x76,0x00,0x00,0x01,0x02 })
-				.ExpectStatusBytes("9000");
+			return this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xa4,0x04,0x0c,0x06,0xd2,0x76,0x00,0x00,0x01,0x02 })
+				.CheckStatusBytes(selectFileStatusBytes)
+				.GetStatusBytes();
 		}
 
+		/// <summary> Selektieren der KVK-Chipkartenanwendung </summary>
+		public string SelectKVK()
+		{
+			return this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xa4,0x04,0x00,0x06,0xd2,0x76,0x00,0x00,0x01,0x01 })
+				.CheckStatusBytes(selectFileStatusBytes)
+				.GetStatusBytes();
+		}
 
-		/// <summary> VST-Template und VD-Template lesen </summary>
+		private static readonly Dictionary<string,string> selectFileStatusBytes = new Dictionary<string,string>
+		{
+			{ "9000", "Command successful erfolgreiche Selektion eines Files" },
+			{ "6283", "Warning: FileDeactivated selektiertes File ist logisch oder physikalisch deaktiviert" },
+			{ "6a82", "Error: FileNotFound zu selektierendes File wurde nicht gefunden" },
+			{ "6900", "Error: Command not allowed Mobiles Kartenterminal: Autorisierung fehlt" },
+		};
+
+
+		/// <summary> Lesen des VST- und VD-Datenbereichs </summary>
 		public EgkResult ReadEGK()
 		{
 			byte[] pdData = this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xb0,0x81,0x00,0x00,0x00,0x00 })
-				.ExpectStatusBytes("9000","6282");
+				.CheckStatusBytes(readBinaryStatusBytes);
 
 			byte[] vdData = this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xb0,0x82,0x00,0x00,0x00,0x00 })
-				.ExpectStatusBytes("9000","6282","6f00");
+				.CheckStatusBytes(readBinaryStatusBytes);
 
 			return new EgkResult(pdData,vdData);
 		}
 
-
-		/// <summary> KVK-Applikation selektieren </summary>
-		public void SelectKVK()
-		{
-			this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xa4,0x04,0x00,0x06,0xd2,0x76,0x00,0x00,0x01,0x01 })
-				.ExpectStatusBytes("9000");
-		}
-
-		/// <summary> KVK-Template lesen </summary>
+		/// <summary> Lesen des VST- und VD-Datenbereichs </summary>
 		public KvkResult ReadKVK()
 		{
 			byte[] kvkData = this.ExecuteCommand(sad: 2,dad: 0,command: new byte[] { 0x00,0xb0,0x00,0x00,0x00 })
-				.ExpectStatusBytes("9000","6282");
+				.CheckStatusBytes(readBinaryStatusBytes);
 
 			return new KvkResult(kvkData);
 		}
+
+		private static readonly Dictionary<string,string> readBinaryStatusBytes = new Dictionary<string,string>
+		{
+			{ "9000", "Command successful" },
+			{ "6282", "Warning: EndOfFileWarning weniger Daten vorhanden, als mittels Ne angefordert" },
+			{ "6281", "Warning: CorruptDataWarning möglicherweise sind die Antwortdaten korrupt" },
+			{ "6700", "Error: WrongLength Die Anzahl der angeforderten Daten übersteigt die maximale Puffergröße." },
+			{ "6a82", "Error: FileNotFound per shortFileIdentifier adressiertes EF nicht gefunden" },
+			{ "6986", "Error: NoCurrentEF es ist kein EF ausgewählt" },
+			{ "6982", "Error: SecurityStatusNotSatisfied Zugriffsregel nicht erfüllt" },
+			{ "6981", "Error: WrongFileType ausgewähltes EF ist nicht transparent" },
+			{ "6b00", "Error: OffsetTooBig Parameter offset in Kommando APDU ist zu groß" },
+			{ "6900", "Error: Command not allowed Mobiles Kartenterminal: Autorisierung fehlt" },
+		};
 	}
 }
--- KaupischIT.CardReader/Extensions.cs
+++ KaupischIT.CardReader/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace KaupischIT.CardReader
 {
@@ -24,14 +25,57 @@ namespace KaupischIT.CardReader
 				return bytes[bytes.Length-2].ToHexString()+bytes[bytes.Length-1].ToHexString();
 		}
 
-		public static byte[] ExpectStatusBytes(this byte[] bytes,params string[] hexReturnCodes)
+
+		/// <summary>
+		/// Überprüft das Zwei-Byte-Statuswort am Ende der zurückgelieferten Antwort der CT-API.
+		/// </summary>
+		/// <param name="bytes">die Bytefolge, deren Trailer ein Zwei-Byte-Statuswort(SW1-SW2) beinhaltet, dessen Kodierung in Anlehnung an ISO 7816-4 erfolgt.</param>
+		/// <param name="specificReturnCodes">eine Auflistung spezifischer Status-Codes und deren Bedeutung</param>
+		/// <returns>die übergebene Bytefolge</returns>
+		public static byte[] CheckStatusBytes(this byte[] bytes,IDictionary<string,string> specificReturnCodes)
 		{
 #if DEBUG
-			string actual = bytes.GetStatusBytes();
-			string methodName = new StackTrace().GetFrame(1).GetMethod().Name;
-			bool isWarning = !hexReturnCodes.Contains(actual);
+			Dictionary<string,string> generalStatusBytes = new Dictionary<string,string>
+			{
+				{ "64a1", "Error: Keine Karte vorhanden" },
+				{ "64a2", "Error: Karte nicht aktiviert" },
+				{ "6700", "Error: Falsche Länge / Zu viele oder zu wenig Daten (Objekte) im Kommando enthalten." },
+				{ "6900", "Error: Kommando (ggf. z.Zt.) nicht zulässig" },
+				{ "6930", "Error: Das Terminal unterstützt (für diesen Befehl) keine Timer-Option." },
+				{ "6940", "Error: Das Terminal unterstützt (für diesen Befehl) keine Display-Option." },
+				{ "6941", "Error: Funktionseinheit belegt / nicht verfügbar" },
+				{ "6942", "Error: Das Display unterstützt den selektierten Zeichensatz nicht." },
+				{ "6a00", "Error: Falsche Parameter P1, P2" },
+				{ "6a80", "Error: Unzulässige Parameter (Datenobjekt) im Datenfeld" },
+				{ "6a88", "Error: Referenzdaten (Datenobjekt) nicht in Datenteil des Kommandos enthalten." },
+				{ "6a89", "Error: Zu viele Datenobjekte gleichen Typs sind enthalten." },
+				{ "6c00", "Error: Falsche Längenangabe Le" },
+				{ "6d00", "Error: Falsches/unbekanntes Instruction-Byte" },
+				{ "6e00", "Error: Falsches/unbekanntes Class-Byte" },
+				{ "6f00", "Error: Kommunikationsfehler zur Karte" },
+			};
 
-			Debug.WriteLine(methodName.PadRight(12)+" "+actual+" (expected: "+String.Join(", ",hexReturnCodes)+")",(isWarning) ? "Warning" : null);
+			// den Namen der Methode ermitteln, aus der die CheckStatusBytes-Methode aufgerufen wurde
+			string methodName = new StackTrace().GetFrame(1).GetMethod().Name;
+
+			string statusBytes = bytes.GetStatusBytes();
+			if (statusBytes!=null)
+			{
+				/// Das erste Byte des Trailers (= das vorletzte Byte in der Bytefolge) SW1 kodiert stets den Ausgang einer Operation
+				string sw1 = statusBytes.Substring(0,2);
+				string responseStatus =
+					(new[] { "90","61" }.Contains(sw1)) ? "Process completed - Normal Processing" :
+					(new[] { "62","63" }.Contains(sw1)) ? "Process completed - Warning" :
+					(new[] { "64","65" }.Contains(sw1)) ? "Process aborted - Execution Error" :
+					(new[] { "67","68","69","6a","6b","6c","6d","6e","6f" }.Contains(sw1)) ? "Process aborted - Checking Error" : "";
+
+				// in den Auflistungen der erwarteten Statuscodes nachschauen, ob es eine Beschreibung gibt
+				string description = ((specificReturnCodes.TryGetValue(statusBytes,out string value) || generalStatusBytes.TryGetValue(statusBytes,out value)) ? value : responseStatus);
+
+				Debug.WriteLine(methodName.PadRight(12) + statusBytes + ((description!=null)?" \t("+description+")":""));
+			}
+			else
+				Debug.WriteLine(methodName.PadRight(12)+"Response Trailer vorhanden");
 #endif
 			return bytes;
 		}
